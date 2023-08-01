564d4d67f69c59459048b2d4aee88396c80dccaf
dohi0512 <donghuni512@gmail.com>
2023-08-01 21:38:46 +0900

    Style:: 디자인 변경

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,17 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
+import { BsFillMoonFill } from "react-icons/bs"
 const ThemeButton = () => {
+  const [moveX, setMoveX] = useState(0)
   return (
     <ThemeToggler>
       {({ theme, toggleTheme }) => (
-        <label>
-          <input
-            type="checkbox"
-            onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-            checked={theme === "dark"}
-          />{" "}
-          Dark mode
-        </label>
+        <div
+          className="w-24 h-8 rounded-2xl bg-white flex flex-row items-center relative justify-end"
+          onClick={() => {
+            toggleTheme(theme === "light" ? "dark" : "light")
+            setMoveX(moveX === 0 ? -1.6 : 0)
+          }}
+        >
+          <div
+            className="w-full h-full flex items-center rounded-2xl bg-black justify-between z-10"
+            style={{ transform: `translateX(${moveX}rem)` }}
+          >
+            <BsFillMoonFill size={"1rem"} color="white" className="ml-3" />
+            <span className="text-sm mr-3 text-white">다크모드</span>
+          </div>
+          <span className="absolute text-xs text-black mr-2">On</span>
+        </div>
       )}
     </ThemeToggler>
   )
