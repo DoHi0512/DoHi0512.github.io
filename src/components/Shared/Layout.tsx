0c8ca9b54eecb48b71ad794d797bf5dbb6ad7de3
dohi0512 <donghuni512@gmail.com>
2023-08-17 20:36:58 +0900

    Refactoring:: SEO최적화

--- src/components/Shared/Layout.tsx
+++ src/components/Shared/Layout.tsx
@@ -1,14 +1,18 @@
 import * as React from "react"
 import Header from "./Header"
+import Seo from "../seo"
 
 const Layout = ({ children }: any) => {
   return (
-    <div className="w-full flex justify-center duration-300 pb-12 bg-bg-color text-text-color min-h-screen ">
-      <div className="w-4/5">
-        <Header width="full" />
-        {children}
+    <>
+      <Seo title="DoHi.log" description="DoHi's TechBlog" />
+      <div className="w-full flex justify-center duration-300 pb-12 bg-bg-color text-text-color min-h-screen ">
+        <div className="w-4/5">
+          <Header width="full" />
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/components/seo.js
+++ src/components/seo.js
@@ -1,14 +1,8 @@
-/**
- * SEO component that queries for data with
- * Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
+import { Helmet } from "react-helmet"
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-const Seo = ({ description, title, children }) => {
+const Seo = ({ description, title }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -17,7 +11,7 @@ const Seo = ({ description, title, children }) => {
             title
             description
             social {
-              twitter
+              instagram
             }
           }
         }
@@ -29,21 +23,23 @@ const Seo = ({ description, title, children }) => {
   const defaultTitle = site.siteMetadata?.title
 
   return (
-    <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+    <Helmet title={defaultTitle ? `${title} | ${defaultTitle}` : title}>
       <meta name="description" content={metaDescription} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
+      <meta name="instagram:card" content="summary" />
       <meta
-        name="twitter:creator"
-        content={site.siteMetadata?.social?.twitter || ``}
+        name="instagram:creator"
+        content={site.siteMetadata?.social?.instagram || ``}
       />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
-      {children}
-    </>
+      <meta
+        name="google-site-verification"
+        content={process.env.GATSBY_SITE_KEY}
+      />
+      <meta name="instagram:title" content={title} />
+      <meta name="instagram:description" content={metaDescription} />
+    </Helmet>
   )
 }
 
