6a0272c2041d60e7da702cf7cdb172dc1882be61
dohi0512 <donghuni512@gmail.com>
2023-08-18 14:06:19 +0900

    Refactoring:: 이미지 방식 변경

---
+++ src/components/Main/post/PostItem.tsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { IPostFrontmatter } from "../../../types/PostItem.type"
+import { Link } from "gatsby"
+import Tag from "../Tag"
+import { GatsbyImage } from "gatsby-plugin-image"
+interface IPostItemProps extends IPostFrontmatter {
+  link: string
+  timeToRead: number
+}
+export const PostItem = ({
+  title,
+  date,
+  description,
+  tags,
+  thumbnail: {
+    childImageSharp: { gatsbyImageData },
+  },
+  link,
+  timeToRead,
+}: IPostItemProps) => {
+  console.log(timeToRead)
+  return (
+    <Link
+      to={link}
+      className="w-full flex flex-col  shadow-lg items-center bg-bg-color2 rounded-lg hover:translate-y-[-.8rem] duration-300"
+    >
+      <GatsbyImage
+        alt="img"
+        image={gatsbyImageData}
+        className="w-full object-cover h-[15rem] rounded-t-lg"
+      />
+      <div className="w-[85%] flex flex-col ">
+        <span className="text-2xl font-bold text-omit mt-4">{title}</span>
+        <span className="text-excerpt mt-2">{description}</span>
+        <div className="flex flex-row mt-6">
+          {tags?.map((tag, idx) => (
+            <Tag tag={tag} key={idx} />
+          ))}
+        </div>
+        <span className="flex justify-between w-full pb-4 mt-4">
+          <span>{date}</span>
+          <span>{timeToRead} min read</span>
+        </span>
+      </div>
+    </Link>
+  )
+}
+
+export default PostItem
--- src/components/Post/PostHeader.tsx
+++ src/components/Post/PostHeader.tsx
@@ -1,19 +1,28 @@
+import { GatsbyImage } from "gatsby-plugin-image"
 import React from "react"
 import { IPostFrontmatter } from "src/types/PostItem.type"
 
-const PostHeader = ({ title, thumbnail, date, tags }: IPostFrontmatter) => {
+const PostHeader = ({
+  title,
+  thumbnail: {
+    childImageSharp: { gatsbyImageData },
+  },
+  date,
+  tags,
+}: IPostFrontmatter) => {
   return (
     <div className="h-[30rem] w-full relative flex justify-center items-center bg-black">
-      <img
+      <GatsbyImage
+        image={gatsbyImageData}
         className="w-full h-full object-cover absolute opacity-40"
-        src={thumbnail}
+        alt="img"
       />
       <div className="absolute flex flex-col items-center">
         <span className="lg:text-4xl text-3xl text-white font-bold">
           {title}
         </span>
         <span className="text-sm text-white mt-2">{date}</span>
-        <span className="text-sm text-white mt-2">{tags.join("/")}</span>
+        <span className="text-sm text-white mt-2">{tags?.join("/")}</span>
       </div>
     </div>
   )
