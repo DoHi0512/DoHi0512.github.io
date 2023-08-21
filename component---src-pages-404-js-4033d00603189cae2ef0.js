35aedf4e88eb1f17c815b9f924ed4f3f1a0bcaa9
이동훈 <202110309@bssm.hs.kr>
2023-08-21 16:04:14 +0900

    Updates

--- component---src-pages-404-js-4033d00603189cae2ef0.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{429:function(t,e,a){a.r(e);var l=a(7294),s=a(2707);e.default=t=>{let{data:e,location:a}=t;e.site.siteMetadata.title;return l.createElement(s.Z,null,l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-4033d00603189cae2ef0.js.map
