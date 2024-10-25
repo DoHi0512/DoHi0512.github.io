3e4143142bb016e67bd1b7140e24549f65b58996
dohi0512 <donghuni512@gmail.com>
2024-10-25 20:52:41 +0900

    Fix:: hydrate error

--- src/components/Main/recent/RecentPostList.tsx
+++ src/components/Main/recent/RecentPostList.tsx
@@ -1,20 +1,32 @@
-import React, { useRef } from "react"
+import React, { useEffect, useState } from "react"
 import Slider from "react-slick"
 import RecentPostLayout from "./RecentPostLayout"
 import { IPostItem } from "src/types/PostItem.type"
 import useWindowSize from "../../../hooks/useWindowSize"
+
 export const RecentPostList = ({ postList }: { postList: IPostItem[] }) => {
   const { width } = useWindowSize(0)
+  const [slidesToShow, setSlidesToShow] = useState(1.5)
+
+  useEffect(() => {
+    if (width) {
+      if (width <= 767) setSlidesToShow(0.3)
+      else if (width <= 1023) setSlidesToShow(1)
+      else setSlidesToShow(1.5)
+    }
+  }, [width])
+
   const settings = {
-    className: "w-full  mt-16 duration-300",
+    className: "w-full mt-16 duration-300",
     dots: true,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 5000,
-    slidesToShow: width <= 767 ? 0.3 : width <= 1023 ? 1 : 1.5,
+    slidesToShow: slidesToShow,
     centerMode: true,
     arrows: false,
   }
+
   return (
     <Slider {...settings}>
       <RecentPostLayout option={1} postList={postList.slice(0, 3)} />
--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,35 +1,24 @@
 import { useEffect, useState } from "react"
+
 export const useWindowSize = (delay: number) => {
-  const [windowSize, setWindowSize] = useState<{
-    width: number
-    height: number
-  }>({
-    width: typeof window !== "undefined" ? window.innerWidth : 0,
-    height: typeof window !== "undefined" ? window.innerHeight : 0,
-  })
+  const [width, setWidth] = useState<number | null>(null)
 
   useEffect(() => {
-    let timeoutId: NodeJS.Timeout
-
     const handleResize = () => {
-      clearTimeout(timeoutId)
-      timeoutId = setTimeout(() => {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        })
-      }, delay)
+      setWidth(window.innerWidth)
     }
 
-    window.addEventListener("resize", handleResize)
+    if (typeof window !== "undefined") {
+      handleResize()
+      window.addEventListener("resize", handleResize)
+    }
 
     return () => {
       window.removeEventListener("resize", handleResize)
-      clearTimeout(timeoutId)
     }
   }, [delay])
 
-  return windowSize
+  return { width }
 }
 
 export default useWindowSize
