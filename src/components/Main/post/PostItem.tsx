49f858045362f0b8f3bfbf48384177c77eaf8bcc
dohi0512 <donghuni512@gmail.com>
2024-10-25 21:24:25 +0900

    Chore:: 정렬 변경

--- src/components/Main/post/PostItem.tsx
+++ src/components/Main/post/PostItem.tsx
@@ -32,9 +32,7 @@ export const PostItem = ({
         <span className="text-xl font-bold text-omit mt-4 ">{title}</span>
         <span className="text-excerpt mt-2 h-[3rem]">{description}</span>
         <div className="flex flex-row mt-6">
-          {tags?.map((tag, idx) => (
-            <Tag tag={tag} key={idx} />
-          ))}
+          {tags?.map((tag, idx) => <Tag tag={tag} key={idx} />)}
         </div>
         <span className="flex justify-between w-full pb-4 mt-4">
           <span>{date}</span>
